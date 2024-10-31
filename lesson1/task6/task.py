4c42785757f6dafcd43c1633426f9af7d4019288
Tadas <tadasa@gmail.com>
2024-10-31 18:38:01 +0000

    Add 5 and 6 lesson with a all in one file

---
+++ lesson1/task6/task.py
@@ -0,0 +1,13 @@
+# Create a Python tuple with at least five elements. Perform operations like accessing an element and finding the index of an
+# element. Remember, tuples are immutable so operations like adding or removing an element are not possible.
+
+# Create a tuple with five elements
+my_tuple = (10, 20, 30, 40, 50, 60)
+
+# Access the fourth element
+fourth_element = my_tuple[4]
+print(fourth_element)
+
+# Find the index of element 40
+index_of_40 = my_tuple.index(40)
+print(index_of_40)
